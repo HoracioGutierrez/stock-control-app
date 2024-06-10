f60c53bdf643a17cc5dea292e0e826976d2b1f2f
Horacio Gutierrez <horacio.estevez@gmail.com>
2024-06-09 23:52:47 -0300

    added new block styling

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,18 @@
 import { auth } from "@/auth";
+import { cn } from "@/lib/utils";
 
 export default async function Home() {
 
   const session = await auth()
 
   return (
-    <main className="p-4">
-      Home
+    <main className="grid grid-cols-1 md:grid-cols-[300px_1fr] grow">
+      <div id="sidebar" className={cn("hidden md:block p-4 h-full", session && "border-r border-slate-400 bg-primary-foreground")}>
+        sidebar
+      </div>
+      <div id="content" className="p-4">
+        content
+      </div>
     </main>
   );
 }
--- components/Header.tsx
+++ components/Header.tsx
@@ -1,18 +1,26 @@
 import { auth } from "@/auth"
 import LogoutButton from "./LogoutButton"
 import { ModeToggle } from "./ModeToggle"
+import { Menu, PackageOpen } from "lucide-react"
+import { cn } from "@/lib/utils"
 
 async function Header() {
 
   const session = await auth()
 
   return (
-    <header className="p-4 flex justify-between">
-      <h1 className="text-4xl font-bold">Control de Stock</h1>
-      <nav className="gap-8 flex">
-        {session && <LogoutButton />}
-        <ModeToggle />
-      </nav>
+    <header className={cn("justify-between grid grid-cols-1 md:grid-cols-[300px_1fr]", session && "bg-primary-foreground border-b border-slate-400")}>
+      <div className={cn("items-center gap-2 hidden md:flex p-4", session && "border-r border-slate-400")}>
+        <PackageOpen className="h-6 w-6 " />
+        <h1 className="text-2xl font-bold">Control de Stock</h1>
+      </div>
+      <div className="md:justify-self-end flex justify-between justify-self-auto items-center p-4">
+        <Menu className="h-6 w-6 md:hidden" />
+        <nav className="gap-8 flex ">
+          {session && <LogoutButton />}
+          <ModeToggle />
+        </nav>
+      </div>
     </header>
   )
 }

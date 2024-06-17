048793cabb028d3fc359e6b12e7db72f4e8881a4
Horacio Gutierrez <horacio.estevez@gmail.com>
2024-06-17 02:12:22 -0300

    add some new config to sw strategy

--- components/ThemeProvider.tsx
+++ components/ThemeProvider.tsx
@@ -7,3 +7,7 @@ import { type ThemeProviderProps } from "next-themes/dist/types"
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
   return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
+
+if (typeof navigator !== undefined && navigator.serviceWorker) {
+  navigator.serviceWorker.register("/sw.js");
+}
--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,30 @@ import withPWA from "next-pwa";
 const withPWAWrapper = withPWA({
   dest: "public",
   register: true,
+  cacheOnFrontEndNav : true,
+  runtimeCaching : [
+    {
+      urlPattern : /\/products\/(.*)/,
+      handler : "CacheFirst",
+      options : {
+        cacheName : "products-cache"
+      }
+    },
+    {
+      urlPattern : /\/_next\/static\/(.*)/,
+      handler : "CacheFirst",
+      options : {
+        cacheName : "static-cache"
+      }
+    }, 
+    {
+      urlPattern : /\/(manifest|favicon|icons)\.(.*)/,
+      handler : "CacheFirst",
+      options : {
+        cacheName : "manifest-cache"
+      }
+    }
+  ]
 });
 
 /** @type {import('next').NextConfig} */

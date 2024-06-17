59b74ee4bc1978449b42c1684524cdd73941296a
Horacio Gutierrez <horacio.estevez@gmail.com>
2024-06-17 01:11:17 -0300

    add pwa integration library

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
+import withPWA from "next-pwa";
+/* const withPWA = require('next-pwa')({
+  dest: 'public'
+}) */
+
+const withPWAWrapper = withPWA({
+  dest: "public",
+  register: true,
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+export default withPWAWrapper(nextConfig);

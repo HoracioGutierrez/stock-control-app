359e386e3544bd281127b60ef84aa9710590f46f
Horacio Gutierrez <horacio.estevez@gmail.com>
2024-06-17 02:17:49 -0300

    change sw registration

--- components/ThemeProvider.tsx
+++ components/ThemeProvider.tsx
@@ -5,9 +5,14 @@ import { ThemeProvider as NextThemesProvider } from "next-themes"
 import { type ThemeProviderProps } from "next-themes/dist/types"
 
 export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
-}
 
-if (typeof navigator !== undefined && navigator.serviceWorker) {
-  navigator.serviceWorker.register("/sw.js");
+  React.useEffect(() => {
+    if (typeof window !== "undefined") {
+      if (typeof navigator !== undefined && navigator.serviceWorker) {
+        navigator.serviceWorker.register("/sw.js");
+      }
+    }
+  }, [])
+
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }

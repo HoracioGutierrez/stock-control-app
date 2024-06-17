d71ba013c8e84935decd70e622c0b907ded5a288
Horacio Gutierrez <horacio.estevez@gmail.com>
2024-06-17 12:53:25 -0300

    try new pwa library ootb

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,152 @@
-import withPWA from "next-pwa";
+const runtimeCaching = {
+  workboxOptions: {
+    runtimeCaching: [
+      {
+        urlPattern: /^https:\/\/fonts\.(?:gstatic)\.com\/.*/i,
+        handler: "CacheFirst",
+        options: {
+          cacheName: "google-fonts-webfonts",
+          expiration: {
+            maxEntries: 4,
+            maxAgeSeconds: 365 * 24 * 60 * 60, // 365 days
+          },
+        },
+      },
+      {
+        urlPattern: /^https:\/\/fonts\.(?:googleapis)\.com\/.*/i,
+        handler: "StaleWhileRevalidate",
+        options: {
+          cacheName: "google-fonts-stylesheets",
+          expiration: {
+            maxEntries: 4,
+            maxAgeSeconds: 7 * 24 * 60 * 60, // 7 days
+          },
+        },
+      },
+      {
+        urlPattern: /\.(?:eot|otf|ttc|ttf|woff|woff2|font.css)$/i,
+        handler: "StaleWhileRevalidate",
+        options: {
+          cacheName: "static-font-assets",
+          expiration: {
+            maxEntries: 4,
+            maxAgeSeconds: 7 * 24 * 60 * 60, // 7 days
+          },
+        },
+      },
+      {
+        urlPattern: /\.(?:jpg|jpeg|gif|png|svg|ico|webp)$/i,
+        handler: "StaleWhileRevalidate",
+        options: {
+          cacheName: "static-image-assets",
+          expiration: {
+            maxEntries: 64,
+            maxAgeSeconds: 30 * 24 * 60 * 60, // 30 days
+          },
+        },
+      },
+      {
+        urlPattern: /\/_next\/static.+\.js$/i,
+        handler: "CacheFirst",
+        options: {
+          cacheName: "next-static-js-assets",
+          expiration: {
+            maxEntries: 64,
+            maxAgeSeconds: 24 * 60 * 60, // 24 hours
+          },
+        },
+      },
+      {
+        urlPattern: /\/_next\/image\?url=.+$/i,
+        handler: "StaleWhileRevalidate",
+        options: {
+          cacheName: "next-image",
+          expiration: {
+            maxEntries: 64,
+            maxAgeSeconds: 24 * 60 * 60, // 24 hours
+          },
+        },
+      },
+      {
+        urlPattern: /\.(?:mp3|wav|ogg)$/i,
+        handler: "CacheFirst",
+        options: {
+          rangeRequests: true,
+          cacheName: "static-audio-assets",
+          expiration: {
+            maxEntries: 32,
+            maxAgeSeconds: 24 * 60 * 60, // 24 hours
+          },
+        },
+      },
+      {
+        urlPattern: /\.(?:mp4|webm)$/i,
+        handler: "CacheFirst",
+        options: {
+          rangeRequests: true,
+          cacheName: "static-video-assets",
+          expiration: {
+            maxEntries: 32,
+            maxAgeSeconds: 24 * 60 * 60, // 24 hours
+          },
+        },
+      },
+      {
+        urlPattern: /\.(?:js)$/i,
+        handler: "StaleWhileRevalidate",
+        options: {
+          cacheName: "static-js-assets",
+          expiration: {
+            maxEntries: 48,
+            maxAgeSeconds: 24 * 60 * 60, // 24 hours
+          },
+        },
+      },
+      {
+        urlPattern: /\.(?:css|less)$/i,
+        handler: "StaleWhileRevalidate",
+        options: {
+          cacheName: "static-style-assets",
+          expiration: {
+            maxEntries: 32,
+            maxAgeSeconds: 24 * 60 * 60, // 24 hours
+          },
+        },
+      },
+      {
+        urlPattern: /\/_next\/data\/.+\/.+\.json$/i,
+        handler: "StaleWhileRevalidate",
+        options: {
+          cacheName: "next-data",
+          expiration: {
+            maxEntries: 32,
+            maxAgeSeconds: 24 * 60 * 60, // 24 hours
+          },
+        },
+      },
+      {
+        urlPattern: /\.(?:json|xml|csv)$/i,
+        handler: "NetworkFirst",
+        options: {
+          cacheName: "static-data-assets",
+          expiration: {
+            maxEntries: 32,
+            maxAgeSeconds: 24 * 60 * 60, // 24 hours
+          },
+        },
+      },
+    ],
+  },
+};
+
+/* import withPWA from "next-pwa";
 
 const withPWAWrapper = withPWA({
   dest: "public",
   register: true,
   cacheOnFrontEndNav: true,
   disableDevLogs: true,
+  
   runtimeCaching: [
     { urlPattern: "/products", handler: "CacheFirst", options: { cacheName: "products-cache" } },
     { urlPattern: "/customers", handler: "CacheFirst", options: { cacheName: "customers-cache" } },
@@ -29,9 +171,28 @@ const withPWAWrapper = withPWA({
     },
   ],
   buildExcludes: ["app-build-manifest.json"],
+}); */
+
+import withSerwistInit from "@serwist/next";
+
+const withSerwist = withSerwistInit({
+  // Note: This is only an example. If you use Pages Router,
+  // use something else that works, such as "service-worker/index.ts".
+  swSrc: "app/sw.ts",
+  swDest: "public/sw.js",
+  cacheOnNavigation: true,
+  //aggressiveFrontEndNavCaching: true,
+  
+  exclude: [
+    ({ asset }) => {
+      return true;
+    },
+  ],
 });
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default withPWAWrapper(nextConfig);
+//export default withPWAWrapper(nextConfig);
+//export default nextConfig;
+export default withSerwist(nextConfig);

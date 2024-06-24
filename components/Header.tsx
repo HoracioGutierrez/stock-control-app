2290a3d0a4257dfcd75177dda563b65b3458dc20
Horacio Gutierrez <horacio.estevez@gmail.com>
2024-06-24 16:31:26 -0300

    fix dependency

--- components/Header.tsx
+++ components/Header.tsx
@@ -11,13 +11,13 @@ async function Header() {
   return (
     <header className={cn("justify-between grid grid-cols-1 md:grid-cols-[300px_1fr]", session && "bg-primary-foreground border-b border-accent")}>
       <div className={cn("items-center gap-2 hidden md:flex p-4", session && "border-r border-accent justify-center flex-col")}>
-        <PackageOpen className="h-6 w-6 " />
-        <h1 className="text-2xl font-bold">Control de Stock</h1>
+        <PackageOpen className="w-6 h-6" />
+        <h1 className="font-bold text-2xl">Control de Stock</h1>
       </div>
-      <div className="md:justify-self-end flex justify-between justify-self-auto items-center p-4">
-        <Menu className="h-6 w-6 md:hidden" />
-        <nav className="gap-8 flex ">
-          {session && <LogoutButton />}
+      <div className="flex justify-between justify-self-auto md:justify-self-end items-center p-4">
+        <Menu className="md:hidden w-6 h-6" />
+        <nav className="flex gap-8">
+          {session && <LogoutButton collapsed={false}/>}
           <ModeToggle />
         </nav>
       </div>

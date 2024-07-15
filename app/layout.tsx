1cef2cb42eda90c971eed086df93be8c3aa7c4f6
Horacio Gutierrez <horacio.estevez@gmail.com>
2024-07-15 16:06:04 -0300

    add default serwist settings

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,17 +2,56 @@ import { ThemeProvider } from "@/components/ThemeProvider";
 import { Toaster } from "@/components/ui/toaster";
 import { SessionProvider } from "next-auth/react";
 import { Inter } from "next/font/google";
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import { cn } from "@/lib/utils";
 import "./globals.css";
-import CustomTour from "@/components/layout/CustomTour";
-
 
 const inter = Inter({ subsets: ["latin"] });
 
+const APP_NAME = "Stock Control App";
+const APP_DEFAULT_TITLE = "Stock Control";
+const APP_TITLE_TEMPLATE = "%s - PWA";
+const APP_DESCRIPTION = "A stock control system for the company";
+
 export const metadata: Metadata = {
-  title: "Stock Control",
-  description: "A stock control system for the company",
+  //title: "Stock Control",
+  //description: "A stock control system for the company",
+  applicationName: APP_NAME,
+  title: {
+    default: APP_DEFAULT_TITLE,
+    template: APP_TITLE_TEMPLATE,
+  },
+  description: APP_DESCRIPTION,
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: APP_DEFAULT_TITLE,
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: APP_NAME,
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: APP_DEFAULT_TITLE,
+      template: APP_TITLE_TEMPLATE,
+    },
+    description: APP_DESCRIPTION,
+  }
+};
+
+export const viewport: Viewport = {
+  themeColor: "#FFFFFF",
 };
 
 
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,18 @@
+import withSerwistInit from "@serwist/next";
+
+const withSerwist = withSerwistInit({
+  // Note: This is only an example. If you use Pages Router,
+  // use something else that works, such as "service-worker/index.ts".
+  swSrc: "app/sw.ts",
+  swDest: "public/sw.js",
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default nextConfig;
+//export default nextConfig;
+
+export default withSerwist({
+  // Your Next.js config
+  ...nextConfig,
+});
